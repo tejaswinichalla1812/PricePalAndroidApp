5fe3f2454fcc0e62446f61cb4a49cc27d88432f2
iamsaikiran <39546922+iamsaikiran@users.noreply.github.com>
2022-03-25 23:43:43 -0500

    Modified compare products java and xml pages with additional functionalities.

--- app/src/main/java/com/app/pricepal/ui/compare/PriceCompareActivity.java
+++ app/src/main/java/com/app/pricepal/ui/compare/PriceCompareActivity.java
@@ -1,23 +1,36 @@
 package com.app.pricepal.ui.compare;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.app.pricepal.R;
 import com.app.pricepal.models.compare_price_model;
+
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class PriceCompareActivity extends AppCompatActivity {
-
+    ImageView itemImg;
+    TextView tvItemName,tvItemQty,tvStoreName,tvPrice,tvDateFilter;
     RecyclerView recyclerView;
     private AdapterComparePriceItem adapterItems;
+
+    final Calendar myCalendar = Calendar.getInstance();
+    private String stDate="01/01/2022";
 
     private final List<compare_price_model> productsList=new ArrayList<>();
     @Override
@@ -33,7 +46,17 @@ public class PriceCompareActivity extends AppCompatActivity {
 
     @SuppressLint("SetTextI18n")
     private void initViews() {
+        itemImg = findViewById(R.id.itemImg);
+        tvItemName = findViewById(R.id.tvItemName);
+        tvItemQty = findViewById(R.id.tvItemQty);
+        tvStoreName = findViewById(R.id.tvStoreName);
+        tvPrice = findViewById(R.id.tvPrice);
         recyclerView = findViewById(R.id.rvPriceList);
+        tvDateFilter = findViewById(R.id.tvDateFilter);
+
+        if (getIntent().getStringExtra("itemName") != null) {
+            tvItemName.setText(getIntent().getStringExtra("itemName"));
+        }
         getProducts();
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
@@ -41,6 +64,23 @@ public class PriceCompareActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapterItems);
         recyclerView.addItemDecoration(new DividerItemDecoration(this,
                 DividerItemDecoration.VERTICAL));
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                stDate=dayOfMonth+"/"+monthOfYear +"/"+year;
+                tvDateFilter.setText("Showing results for- "+stDate);
+                getProducts();
+            }};
+        /*tvDateFilter.setOnClickListener(view -> {
+            DatePickerDialog datePickerDialog=  new DatePickerDialog(PriceCompareActivity.this,R.style.DialogTheme, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH));
+            datePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
+            datePickerDialog.show();
+        });*/
     }
 
     @Override
@@ -59,9 +99,9 @@ public class PriceCompareActivity extends AppCompatActivity {
         for(int i=1;i<5;i++)
         {
             productsList.add(new compare_price_model(
-                    i,"Store "+i,"1 Ltr",
+                    i,"Item "+i,i+"mg",
                     (i*150.00)/5,(i*150.00)/5,
-                    "/img/item1.jpeg",((i*15.00)-1)/5,true));
+                    "/img/item1.jpeg",((i*150.00)-10)/5,true));
         }
     }
 }
--- app/src/main/java/com/app/pricepal/ui/compare/PriceCompareFragment.java
+++ app/src/main/java/com/app/pricepal/ui/compare/PriceCompareFragment.java
@@ -5,22 +5,84 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.app.pricepal.R;
 import com.app.pricepal.databinding.FragmentCompareBinding;
+import com.app.pricepal.ui.stores.StoresViewModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class PriceCompareFragment extends Fragment {
-    private FragmentCompareBinding binding;
 
+    private FragmentCompareBinding binding;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        StoresViewModel storesViewModel =
+                new ViewModelProvider(this).get(StoresViewModel.class);
+
+        String[] items_list = getResources().getStringArray(R.array.items_cat);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Products");
+
         binding = FragmentCompareBinding.inflate(inflater, container, false);
-        Button searchbtn = binding.searchBTN;
-        searchbtn.setOnClickListener(view -> startActivity(new Intent(getContext(),PriceCompareActivity.class)));
         View root = binding.getRoot();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (getContext(),android.R.layout.select_dialog_item,items_list);
+        //Getting the instance of AutoCompleteTextView
+        AutoCompleteTextView actv =  binding.searchText;
+        actv.setThreshold(3);//will start working from first character
+        actv.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
+
+        actv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                startActivity(new Intent(getContext(), PriceCompareActivity.class)
+                        .putExtra("itemName", adapterView.getAdapter().getItem(i).toString())
+                );
+
+            }
+        });
+
+        ArrayList<String> list = new ArrayList<String>();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dsp : dataSnapshot.getChildren())
+                {
+                    if(dsp!= null){
+                        String name = dsp.child("itemName").getValue(String.class);
+                        // Toast.makeText(getContext(), "item:"+name, Toast.LENGTH_SHORT).show();
+                        list.add(name);
+                        actv.setThreshold(3);//will start working from first character
+                        actv.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
         return root;
     }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
