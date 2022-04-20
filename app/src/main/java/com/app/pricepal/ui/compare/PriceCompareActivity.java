9b664cd12b907bd4f1e458714d90e541bfad7714
iamsaikiran <39546922+iamsaikiran@users.noreply.github.com>
2022-04-19 21:18:29 -0500

    Have updated files related to Compare xml and java files.

--- app/src/main/java/com/app/pricepal/ui/compare/PriceCompareActivity.java
+++ app/src/main/java/com/app/pricepal/ui/compare/PriceCompareActivity.java
@@ -4,46 +4,68 @@ import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.DatePicker;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.pricepal.R;
+import com.app.pricepal.main.BaseActivity;
 import com.app.pricepal.models.compare_price_model;
+import com.app.pricepal.models.stores_model;
+import com.app.pricepal.ui.compare.AdapterStores;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class PriceCompareActivity extends AppCompatActivity {
+public class PriceCompareActivity extends BaseActivity {
     ImageView itemImg;
     TextView tvItemName,tvItemQty,tvStoreName,tvPrice,tvDateFilter;
     RecyclerView recyclerView;
     private AdapterComparePriceItem adapterItems;
-
     final Calendar myCalendar = Calendar.getInstance();
-    private String stDate="01/01/2022";
+    private String mainDate="01/01/2022";
+    private String itemName="";
+    private List<compare_price_model> productsList=new ArrayList<>();
+    private List<stores_model> storesList=new ArrayList<>();
+    //    private ArrayList<Integer> storeIds=new ArrayList<>();
+//    private ArrayList<Double> storePrice=new ArrayList<>();
+    private int main_itemId=0;
+    private String main_itemName="";
 
-    private final List<compare_price_model> productsList=new ArrayList<>();
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private TextView textView;
+    private AdapterStores adapterStores;
+    double minPrice=0.00;
+    double maxPrice=0.00;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_price_compare_details);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.teal_700)));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24);
         getSupportActionBar().setTitle("Price Comparison");
+        textView=findViewById(R.id.text_stores);
+        firebaseDatabase = FirebaseDatabase.getInstance();
         initViews();
     }
-
     @SuppressLint("SetTextI18n")
     private void initViews() {
         itemImg = findViewById(R.id.itemImg);
@@ -53,36 +75,30 @@ public class PriceCompareActivity extends AppCompatActivity {
         tvPrice = findViewById(R.id.tvPrice);
         recyclerView = findViewById(R.id.rvPriceList);
         tvDateFilter = findViewById(R.id.tvDateFilter);
-
-        if (getIntent().getStringExtra("itemName") != null) {
-            tvItemName.setText(getIntent().getStringExtra("itemName"));
-        }
-        getProducts();
+        if (getIntent().getStringExtra("itemName") != null)
+            itemName = getIntent().getStringExtra("itemName");
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
-        adapterItems = new AdapterComparePriceItem(this, productsList);
-        recyclerView.setAdapter(adapterItems);
-        recyclerView.addItemDecoration(new DividerItemDecoration(this,
-                DividerItemDecoration.VERTICAL));
-        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                  int dayOfMonth) {
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, monthOfYear);
-                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                stDate=dayOfMonth+"/"+monthOfYear +"/"+year;
-                tvDateFilter.setText("Showing results for- "+stDate);
-                getProducts();
-            }};
-        /*tvDateFilter.setOnClickListener(view -> {
-            DatePickerDialog datePickerDialog=  new DatePickerDialog(PriceCompareActivity.this,R.style.DialogTheme, date, myCalendar
-                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH));
-            datePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
-            datePickerDialog.show();
-        });*/
-    }
 
+        mainDate=myCalendar.get(Calendar.DAY_OF_MONTH)+"/"+(myCalendar.get(Calendar.MONTH)+1)+"/"+myCalendar.get(Calendar.YEAR);
+        tvDateFilter.setText("Showing result for - "+mainDate);
+        getProducts();
+        DatePickerDialog.OnDateSetListener date = (view, year, monthOfYear, dayOfMonth) -> {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            mainDate=dayOfMonth+"/"+(monthOfYear+1) +"/"+year;
+            tvDateFilter.setText("Showing results for - "+mainDate);
+            getProducts();
+        };
+        DatePickerDialog datePickerDialog=  new DatePickerDialog(PriceCompareActivity.this,R.style.DialogTheme, date, myCalendar
+                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.getDatePicker().setMaxDate(new Date().getTime());
+        tvDateFilter.setOnClickListener(view -> {
+            datePickerDialog.show();
+        });
+
+    }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
@@ -95,13 +111,132 @@ public class PriceCompareActivity extends AppCompatActivity {
 
     private void getProducts()
     {
+        showProgressDialog();
+        //searched product
         productsList.clear();
-        for(int i=1;i<5;i++)
+        storesList.clear();
+        minPrice=0.00;
+        maxPrice=0.00;
+
+        databaseReference = firebaseDatabase.getReference("Products");
+        databaseReference.addListenerForSingleValueEvent( new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    try {
+                        String itemName_local = ds.child("itemName").getValue(String.class);
+                        if(itemName_local.equals(itemName))
+                        {
+                            int id = ds.child("id").getValue(Integer.class);
+                            String itemQty = ds.child("itemQty").getValue(String.class);
+                            String itemImg_st = ds.child("itemImg").getValue(String.class);
+                            String itemName_st = ds.child("itemName").getValue(String.class);
+
+                            main_itemId = id;
+                            main_itemName=itemName_st;
+                            tvItemName.setText(itemName);
+                            tvItemQty.setText(itemQty);
+                            Picasso.get()
+                                    .load(itemImg_st)
+                                    .placeholder(R.drawable.storeicon)
+                                    .error(R.drawable.storeicon)
+                                    .into(itemImg);
+                            //prices --> read all stores along with prices
+                            if(main_itemId != 0) {
+                                databaseReference = firebaseDatabase.getReference("Prices");
+                                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                            try {
+                                                String itm_name = ds.child("itemName").getValue(String.class);
+                                                int main_store_id = ds.child("storeId").getValue(int.class);
+
+                                                String st_date = ds.child("date").getValue(String.class);
+                                                if(main_itemName.equals(itm_name) && mainDate.equals(st_date)) {
+                                                    double main_price = ds.child("price").getValue(Double.class);
+                                                    if(main_price < minPrice || minPrice == 0) minPrice = main_price;
+                                                    if(maxPrice < main_price) maxPrice=main_price;
+                                                    //fetch store details
+                                                    databaseReference = firebaseDatabase.getReference("Stores");
+                                                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                                        @Override
+                                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                                            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                                                                try {
+                                                                    int store_id = ds.child("id").getValue(int.class);
+                                                                    if(main_store_id == store_id) {
+                                                                        String name = ds.child("storeName").getValue(String.class);
+                                                                        double loc_lat = ds.child("storeGeoLocationLat").getValue(Double.class);
+                                                                        double loc_lang = ds.child("storeGeoLocationLang").getValue(Double.class);
+                                                                        String img = ds.child("storeImg").getValue(String.class);
+                                                                        String address = ds.child("storeAddress").getValue(String.class);
+                                                                        boolean status = ds.child("storeStatus").getValue(Boolean.class);
+                                                                        storesList.add(new stores_model(
+                                                                                id, name, address,
+                                                                                loc_lang, loc_lat,
+                                                                                img, main_price,status)
+                                                                        );
+                                                                    }
+                                                                } catch (Exception e) {
+                                                                    e.printStackTrace();
+                                                                }
+                                                            }
+                                                            updateUi();
+                                                        }
+
+                                                        @Override
+                                                        public void onCancelled(DatabaseError databaseError) {
+                                                        }
+                                                    });
+                                                }
+                                            } catch (Exception e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+                                    }
+                                });
+                            }
+                        }
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+                updateUi();
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void updateUi()
+    {
+        adapterItems = new AdapterComparePriceItem(this, productsList);
+        recyclerView.setAdapter(adapterItems);
+//        recyclerView.addItemDecoration(new DividerItemDecoration(this,
+//                DividerItemDecoration.VERTICAL));
+        if(storesList.size() !=0 )
         {
-            productsList.add(new compare_price_model(
-                    i,"Item "+i,i+"mg",
-                    (i*150.00)/5,(i*150.00)/5,
-                    "/img/item1.jpeg",((i*150.00)-10)/5,true));
+            textView.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+            recyclerView.setLayoutManager(linearLayoutManager);
+            adapterStores = new AdapterStores(this, storesList);
+            recyclerView.setAdapter(adapterStores);
+            tvPrice.setText("$ "+minPrice +" ~ " +maxPrice);
+        }else
+        {
+            textView.setText("price list not found!");
+            textView.setVisibility(View.VISIBLE);
+            recyclerView.setVisibility(View.GONE);
         }
+        adapterStores = new AdapterStores(this, storesList);
+        recyclerView.setAdapter(adapterStores);
+        hideProgressDialog();
     }
 }
--- app/src/main/java/com/app/pricepal/ui/compare/PriceCompareFragment.java
+++ app/src/main/java/com/app/pricepal/ui/compare/PriceCompareFragment.java
@@ -1,21 +1,35 @@
 package com.app.pricepal.ui.compare;
 
+import static com.app.pricepal.ui.compare.ItemCompareHistory.ITEM_COUNT;
+import static com.app.pricepal.ui.compare.ItemCompareHistory.ITEM_ID;
+import static com.app.pricepal.ui.compare.ItemCompareHistory.ITEM_NAME;
+import static com.app.pricepal.ui.compare.ItemCompareHistory.prefHistKey;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.pricepal.R;
 import com.app.pricepal.databinding.FragmentCompareBinding;
+import com.app.pricepal.models.history_items;
+import com.app.pricepal.ui.items.AdapterItems;
 import com.app.pricepal.ui.stores.StoresViewModel;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -24,68 +38,110 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PriceCompareFragment extends Fragment {
-
     private FragmentCompareBinding binding;
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
+    ArrayList<String> list= new ArrayList<String>();
+    List<String> list_hist= new ArrayList<String>();
+    ListView listview_hist;
+    TextView clearSearchHistory;
+    SharedPreferences sharedpreferences;
+    private RecyclerView recyclerView;
+    private List<history_items> history_items;
+    private AdapterHistory adapterHistory;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         StoresViewModel storesViewModel =
                 new ViewModelProvider(this).get(StoresViewModel.class);
-
-        String[] items_list = getResources().getStringArray(R.array.items_cat);
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Products");
-
         binding = FragmentCompareBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        sharedpreferences = getContext().getSharedPreferences(prefHistKey, Context.MODE_PRIVATE);
+        listview_hist = binding.listHist;
+        clearSearchHistory=binding.clearSearchHistory;
+//        list = binding.list;
+        readHistorydata();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>
-                (getContext(),android.R.layout.select_dialog_item,items_list);
-        //Getting the instance of AutoCompleteTextView
-        AutoCompleteTextView actv =  binding.searchText;
-        actv.setThreshold(3);//will start working from first character
-        actv.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
+                (getContext(), android.R.layout.select_dialog_item, list);
+        AutoCompleteTextView actv = binding.searchText;
+        actv.setThreshold(3);
+        actv.setAdapter(adapter);
 
-        actv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                startActivity(new Intent(getContext(), PriceCompareActivity.class)
-                        .putExtra("itemName", adapterView.getAdapter().getItem(i).toString())
-                );
+        actv.setOnItemClickListener((adapterView, view, i, l) -> {
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+                    int count=sharedpreferences.getInt(ITEM_COUNT,0);
+                    editor.putString(ITEM_NAME+count, adapterView.getAdapter().getItem(i).toString());
+                    editor.putInt(ITEM_ID+count, count);
+                    editor.putInt(ITEM_COUNT, count+1);
+                    //to save our data with key and value.
+                    editor.apply();
+                    readHistorydata();
+                    actv.setText("");
+                    startActivity(new Intent(getContext(), PriceCompareActivity.class)
+                            .putExtra("itemName", adapterView.getAdapter().getItem(i).toString())
+                    );
+                }
+        );
 
-            }
+        clearSearchHistory.setOnClickListener(view -> {
+            ItemCompareHistory itemCompareHistory= new ItemCompareHistory(getContext());
+                itemCompareHistory.clearHistory();
+            readHistorydata();
         });
-
-        ArrayList<String> list = new ArrayList<String>();
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot dsp : dataSnapshot.getChildren())
-                {
-                    if(dsp!= null){
+                for (DataSnapshot dsp : dataSnapshot.getChildren()) {
+                    if (dsp != null) {
                         String name = dsp.child("itemName").getValue(String.class);
-                        // Toast.makeText(getContext(), "item:"+name, Toast.LENGTH_SHORT).show();
                         list.add(name);
-                        actv.setThreshold(3);//will start working from first character
-                        actv.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
+                        actv.setThreshold(3);
+                        actv.setAdapter(adapter);
                         adapter.notifyDataSetChanged();
                     }
                 }
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                        (getContext(), android.R.layout.select_dialog_item, list);
+                AutoCompleteTextView actv = binding.searchText;
+                actv.setThreshold(3);
+                actv.setAdapter(adapter);
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
             }
         });
         return root;
     }
-
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void readHistorydata()
+    {
+        ArrayAdapter<String> arr;
+        ItemCompareHistory itemCompareHistory= new ItemCompareHistory(getContext());
+        try {
+            list_hist = itemCompareHistory.getHistory();
+            arr = new ArrayAdapter<>(
+                    getContext(),
+                    android.R.layout.simple_spinner_dropdown_item,
+                    list_hist);
+            listview_hist.setAdapter(arr);
+            listview_hist.setOnItemClickListener((adapterView, view, i, l) ->
+                    {
+                        startActivity(new Intent(getContext(), PriceCompareActivity.class)
+                                .putExtra("itemName", adapterView.getAdapter().getItem(i).toString())
+                        );
+                    });
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
