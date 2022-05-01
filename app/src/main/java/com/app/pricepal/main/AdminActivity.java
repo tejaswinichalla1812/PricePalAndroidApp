99170fb953d42192f951599728f906d128a7980e
vaidyapusainikhil <89408610+vaidyapusainikhil@users.noreply.github.com>
2022-04-30 20:50:11 -0500

    Have updated few import classes.

--- app/src/main/java/com/app/pricepal/main/AdminActivity.java
+++ app/src/main/java/com/app/pricepal/main/AdminActivity.java
@@ -1,10 +1,22 @@
 package com.app.pricepal.main;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.app.pricepal.MainActivity;
 import com.app.pricepal.R;
+import com.app.pricepal.admin.ManagePrices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class AdminActivity extends AppCompatActivity {
     Button login_btn_admin, loginpage;
