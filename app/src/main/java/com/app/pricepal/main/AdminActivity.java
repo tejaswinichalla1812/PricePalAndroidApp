034109b86fa029bdfb1de793aa97ed8ae4755b24
vaidyapusainikhil <89408610+vaidyapusainikhil@users.noreply.github.com>
2022-04-30 20:14:47 -0500

    Updated the admin email and password details.

--- app/src/main/java/com/app/pricepal/main/AdminActivity.java
+++ app/src/main/java/com/app/pricepal/main/AdminActivity.java
@@ -7,6 +7,12 @@ import android.os.Bundle;
 import com.app.pricepal.R;
 
 public class AdminActivity extends AppCompatActivity {
+    Button login_btn_admin, loginpage;
+    EditText email_et_admin;
+    EditText password_et_admin;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
