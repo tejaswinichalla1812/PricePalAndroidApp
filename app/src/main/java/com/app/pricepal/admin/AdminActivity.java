c5be792f3db872b5058c099a4d3aa6f7d426dfef
tejaswinichalla1812 <tejaswini.challa12@gmail.com>
2022-04-30 23:39:13 -0500

    Modified adminactivity

--- app/src/main/java/com/app/pricepal/admin/AdminActivity.java
+++ app/src/main/java/com/app/pricepal/admin/AdminActivity.java
@@ -1,4 +1,4 @@
-package com.app.pricepal.main;
+package com.app.pricepal.admin;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -6,14 +6,12 @@ import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.pricepal.MainActivity;
 import com.app.pricepal.R;
-import com.app.pricepal.admin.ManagePrices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -33,18 +31,18 @@ public class AdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
-	  login_btn_admin= findViewById(R.id.login_btn_admin);
+        login_btn_admin = findViewById(R.id.login_btn_admin);
         loginpage = findViewById(R.id.login_page);
         email_et_admin = findViewById(R.id.email_et_admin);
         password_et_admin = findViewById(R.id.password_et_admin);
 
         mAuth = FirebaseAuth.getInstance();
 
-	  loginpage.setOnClickListener(new View.OnClickListener() {
+        loginpage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent i = new Intent (AdminActivity.this, MainActivity.class);
+                Intent i = new Intent(AdminActivity.this, MainActivity.class);
                 startActivity(i);
             }
         });
@@ -52,34 +50,26 @@ public class AdminActivity extends AppCompatActivity {
         login_btn_admin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = email_et_admin.getText().toString();
+                String email = email_et_admin.getText().toString().trim();
                 String userpassword = password_et_admin.getText().toString();
-                if(!email.equals("") && !userpassword.equals("")){
-                    signin(email,userpassword);
-                }
-                else{
+                if (!email.equals("") && !userpassword.equals("")) {
+                    signin(email, userpassword);
+                } else {
                     Toast.makeText(AdminActivity.this, "Please enter the Username and Password", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-         public void signin(String email, String password){
-        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Intent i = new Intent (AdminActivity.this,ManagePrices.class);
-                    startActivity(i);
-                    Toast.makeText(AdminActivity.this, "Successful", Toast.LENGTH_SHORT).show();
-                }
-                else{
-                    Toast.makeText(AdminActivity.this, "Unsuccessful", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-
+    public void signin(String email, String userpassword) {
+        if (email.equals("pricepalapp@gmail.com") && userpassword.equals("Pricepal@123")) {
+            Intent i = new Intent(AdminActivity.this, ManagePrices.class);
+            startActivity(i);
+            Toast.makeText(AdminActivity.this, "Login Success!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(AdminActivity.this, "Please enter valid credentials", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
+
